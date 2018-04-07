94a9cdd81fb57d566346698c2ad87c0a2dd20a70
Lindsey Stanek <lstanek4@gmail.com>
2018-04-07 19:59:58 -0400

    feat: added responsive styles for tablet

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -16,26 +16,30 @@ $(document).ready(function() {
   $.each(data, function(i, researchItem) {
     $('.paper-listing').append(
       `<section class="paper-listing__item">
-        ${researchItem.image ? `<img src="${researchItem.image}" alt="${researchItem.altText}">` : ''}
-        ${researchItem.title ? `<h4 class="paper-listing__item__title">${researchItem.title}</h4>` : ''}
-        ${researchItem.authors ? `<p class="paper-listing__item__authors"><em>By: ${researchItem.authors}</em></p>` : ''}
-        ${researchItem.venue ? `<p class="paper-listing__item__venue">${researchItem.venue}</p>` : ''}
-        ${researchItem.date ? `<p class="paper-listing__item__date">${researchItem.date}</p>` : ''}
-        ${researchItem.originalLink ? `<a class="paper-listing__item__link" href="${researchItem.originalLink}">Publisher Website</a>` : ''}
-
-        ${researchItem.abstract ? `
-        <section class="paper-listing__item__more">
-          <p class="paper-listing__item__more__title">Abstract <span>&raquo;</span></p>
-          <p class="paper-listing__item__more__content">${researchItem.abstract}</p>
+        <section class="paper-listing__item__image">
+          ${researchItem.image ? `<img src="${researchItem.image}" alt="${researchItem.altText}">` : ''}
         </section>
-        ` : ''}
+        <section>
+          ${researchItem.title ? `<h4 class="paper-listing__item__title">${researchItem.title}</h4>` : ''}
+          ${researchItem.authors ? `<p class="paper-listing__item__authors"><em>By: ${researchItem.authors}</em></p>` : ''}
+          ${researchItem.venue ? `<p class="paper-listing__item__venue">${researchItem.venue}</p>` : ''}
+          ${researchItem.date ? `<p class="paper-listing__item__date">${researchItem.date}</p>` : ''}
+          ${researchItem.originalLink ? `<a class="paper-listing__item__link" href="${researchItem.originalLink}">Publisher Website</a>` : ''}
 
-        ${researchItem.bibtex ? `
-        <section class="paper-listing__item__more">
-          <p class="paper-listing__item__more__title">Bibtex <span>&raquo;</span></p>
-          <p class="paper-listing__item__more__content"><code>${researchItem.bibtex}</code></p>
+          ${researchItem.abstract ? `
+          <section class="paper-listing__item__more">
+            <p class="paper-listing__item__more__title">Abstract <span>&raquo;</span></p>
+            <p class="paper-listing__item__more__content">${researchItem.abstract}</p>
+          </section>
+          ` : ''}
+
+          ${researchItem.bibtex ? `
+          <section class="paper-listing__item__more">
+            <p class="paper-listing__item__more__title">Bibtex <span>&raquo;</span></p>
+            <p class="paper-listing__item__more__content"><code>${researchItem.bibtex}</code></p>
+          </section>
+          ` : ''}
         </section>
-        ` : ''}
         `);
   });
 
